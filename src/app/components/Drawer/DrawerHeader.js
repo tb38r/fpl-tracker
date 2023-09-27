eaf6e558b555fa549cbfaf320a621c963532d7b1
tb38r <tlawal86@hotmail.co.uk>
2023-09-27 15:10:57 +0100

    minorstyling change

--- src/app/components/Drawer/DrawerHeader.js
+++ src/app/components/Drawer/DrawerHeader.js
@@ -4,7 +4,7 @@ const DrawerHeader = (props) => {
   return (
     <div className=" text-white flex text-justify justify-center h-[4vh] 
     items-center capitalize font-mono font-semibold 
-    md:font-bold lg:font-extrabold w-full text-base md:text-3xl lg:text-4xl drawer-theme">
+    md:font-bold lg:font-extrabold w-full text-sm md:text-2xl lg:text-4xl drawer-theme">
       {props.name}
     </div>
   );
