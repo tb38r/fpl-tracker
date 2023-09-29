db62da35b2e8f48b7045632856cc3509ae9e87ea
Tolu <tlawal86@hotmail.co.uk>
2023-09-29 18:09:01 +0100

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'FPL Tracker',
-  description: 'FPL data source for your decision making',
-}
+  title: "FPL Tracker",
+  description: "FPL data source for your decision making",
+};
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <Analytics />
+      </body>
     </html>
-  )
+  );
 }
