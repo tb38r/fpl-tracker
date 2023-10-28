58b8c360727c850307de08634ae9cdcdea9d0de4
Tolu <tlawal86@hotmail.co.uk>
2023-10-28 11:57:59 +0100

    spurs

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import { GetAllGameweeksData, GetAllPlayerData } from "./utils/helpers";
 
 export const revalidate = 0;
 
-//
+
 export default async function AppMain() {
   
 
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 export async function FetchGameWeekData(gameweek) {
   const response = await fetch(
-    `https://fantasy.premierleague.com/api/event/${gameweek}/live/`,{ cache: 'no-cache' }
+    `https://fantasy.premierleague.com/api/event/${gameweek}/live/`,{ cache: 'no-store' }
     );
 
   if (!response.ok) {
@@ -13,7 +13,7 @@ export async function FetchGameWeekData(gameweek) {
 
 export async function BootstrapStaticData() {
   const response = await fetch(
-    `https://fantasy.premierleague.com/api/bootstrap-static/`, { cache: 'no-cache' }
+    `https://fantasy.premierleague.com/api/bootstrap-static/`, { cache: 'no-store' }
    
   );
 
