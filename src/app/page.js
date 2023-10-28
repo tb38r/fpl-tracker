c3ffdfa2afb0a1ad4bd355d7b3bb9837a840abfc
Tolu <tlawal86@hotmail.co.uk>
2023-10-28 11:53:50 +0100

    spurs

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import {
 } from "@/lib/api";
 import { GetAllGameweeksData, GetAllPlayerData } from "./utils/helpers";
 
-export const revalidate = 1;
+export const revalidate = 0;
 
 //
 export default async function AppMain() {
